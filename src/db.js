69a628f03b9e8955a624d2267654284f36c16a8a
Hoon <si932174@gmail.com>
2022-04-02 21:31:39 +0900

    PORT env variable10

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ mongoose.connect(process.env.DB_URL, {
 }); 
 
 const db = mongoose.connection;
-
+ 
 const handleOpen = () => console.log("✅Connected to DB");
 const handleError = (error) => console.log("❌DB Error", error);
 
